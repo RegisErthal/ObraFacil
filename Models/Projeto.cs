43eef1eb694abf685e11e59c758e3daee835856c
Regis Erthal <regis.erthal.93@gmail.com>
2022-04-24 19:47:14 -0300

    alteracao_fundacao

---
+++ Models/Projeto.cs
@@ -0,0 +1,12 @@
+﻿namespace ObraFAcil.Models
+{
+    public class Projeto
+    {
+        public string NomeProjeto { get; set; }
+        public string EmailCliente { get; set; }
+        public int TamanhoConstrucao { get; set; }
+        public int ValorMetro { get; set; }
+        public DateTime PrevisaoConclusao { get; set; }
+    }
+}
+
