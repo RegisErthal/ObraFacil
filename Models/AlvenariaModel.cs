cc3fe9a39442317f92e4895baea2a03defc1f7f4
Régis Erthal <45245812+RegisErthal@users.noreply.github.com>
2022-04-29 23:07:46 -0300

    ajustes_class_etapas

---
+++ Models/AlvenariaModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace ObraFacil.Models
+{
+    public class AlvenarialModel
+    {
+        public int ParedeM2 { get; set; }
+        public int AlturaBloco { get; set; }
+        public int LarguraBloco { get; set; }
+        public int ProfundidadeBloco { get; set; }
+        public int QuantidadePilares { get; set; }
+        public DateTime PrevisaoEntregaAlvenaria { get; set; }
+    }
+}
