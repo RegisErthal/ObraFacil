8be10f5d4439b0e31f5fbae441b65016e7012bf7
Régis Erthal <45245812+RegisErthal@users.noreply.github.com>
2022-05-18 23:43:27 -0300

    concexao com o banco comentada

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using ObraFacil.Data;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql;
 using ObraFacil;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +10,5 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+//builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
 await builder.Build().RunAsync();
