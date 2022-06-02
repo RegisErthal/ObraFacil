42c5eb75b3906f71dfc8e2a2f82b64fa5fb46d16
Régis Erthal <45245812+RegisErthal@users.noreply.github.com>
2022-06-01 22:18:23 -0300

    testes crud

---
+++ ObraFacil.Infra/Contexts/ObraFacilContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ObraFacil.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObraFacil.Infra.Contexts
+{
+    public class ObraFacilContext : DbContext   
+    {
+        private readonly string _connectionstring;
+
+        public ObraFacilContext()
+        {
+        }
+        public ObraFacilContext(string connectionstring)
+        {
+            _connectionstring = connectionstring;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL(_connectionstring ?? "Server=localhost;Port=3306;Database=dbobrafacil;Uid=root;Pwd=etapas;");
+        }
+
+        public DbSet<AlvenariaModel> Alvenaria { get; set; }
+        public DbSet<CoberturaModel> Cobertura { get; set; }
+        public DbSet<EletricaModel> Eletrica { get; set; }
+        public DbSet<HidraulicaModel> Hidraulica { get; set; }
+        public DbSet<FundacaoModel> Fundacao { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,16 +2,16 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ObraFacil.Data;
 using Microsoft.EntityFrameworkCore;
-using Pomelo.EntityFrameworkCore.MySql;
 using ObraFacil;
-using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using ObraFacil.Infra.Contexts;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),new ServerVersionMysql(new Version(),ServerType.MySql)));
+builder.Services.AddSingleton(new ObraFacilContext(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 await builder.Build().RunAsync();
 
